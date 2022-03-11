6331eb42cf4b29760501cb4dcf543284e18825a8
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-03-11 08:46:01 -0800

    Update ytdl.py

--- mbot/utils/ytdl.py
+++ mbot/utils/ytdl.py
@@ -24,37 +24,210 @@ from asgiref.sync import sync_to_async
 from yt_dlp import YoutubeDL
 from requests import get
 
-@sync_to_async
-def getIds(video):
-    ids = []
-    with YoutubeDL({'quiet':True}) as ydl:
-        info_dict = ydl.extract_info(video, download=False)
-        try:
-            info_dict = info_dict['entries']
-            ids.extend([x.get('id'),x.get('playlist_index'),x.get('creator') or x.get('uploader'),x.get('title'),x.get('duration'),x.get('thumbnail')] for x in info_dict)
-        except:
-            ids.append([info_dict.get('id'),info_dict.get('playlist_index'),info_dict.get('creator') or info_dict.get('uploader'),info_dict.get('title'),info_dict.get('duration'),info_dict.get('thumbnail')])
-    return ids
+YTDL_REGEX = (r"^((?:https?:)?\/\/)"
+              r"?((?:www|m)\.)"
+              r"?((?:youtube\.com|youtu\.be|xvideos\.com|pornhub\.com"
+              r"|xhamster\.com|xnxx\.com))"
+              r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$")
+s2tw = OpenCC('s2tw.json').convert
 
-def audio_opt(path,uploader="@YouNeedMusicBot"):
-    return {
-        "format": "bestaudio",
-        "addmetadata": True,
-        "geo_bypass": True,
-        'noplaylist': True,
-        "nocheckcertificate": True,
-        "outtmpl": f"{path}/%(title)s - {uploader}.mp4",
-        "quiet": True
-    }
+@Jebot.on_message( filters.text
+                   & ~filters.edited
+                   & filters.regex(YTDL_REGEX))
+async def ytdl_with_button(_, message: Message):
+    await message.reply_text(
+        "**Choose download type 🤗**",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Audio 🎵",
+                        callback_data="ytdl_audio"
+                    ),
+                    InlineKeyboardButton(
+                        "Video 🎬",
+                        callback_data="ytdl_video"
+                    )
+                ]
+            ]
+        ),
+        quote=True
+    )
 
-@sync_to_async
-def ytdl_down(opts, url):
-    with YoutubeDL(opts) as ydl:
-        info = ydl.extract_info(url)
-        return ydl.prepare_filename(info)
 
-@sync_to_async
-def thumb_down(videoId):
-    with open(f"/tmp/thumbnails/{videoId}.jpg","wb") as file:
-        file.write(get(f"https://img.youtube.com/vi/{videoId}/default.jpg").content)
-    return f"/tmp/thumbnails/{videoId}.jpg"
+@Jebot.on_callback_query(filters.regex("^ytdl_audio$"))
+async def callback_query_ytdl_audio(_, callback_query):
+    try:
+        url = callback_query.message.reply_to_message.text
+        ydl_opts = {
+            'format': 'bestaudio',
+            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+            'writethumbnail': True
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            message = callback_query.message
+            await message.reply_chat_action("typing")
+            info_dict = ydl.extract_info(url, download=False)
+            # download
+            await callback_query.edit_message_text("**Downloading audio...**")
+            ydl.process_info(info_dict)
+            # upload
+            audio_file = ydl.prepare_filename(info_dict)
+            task = asyncio.create_task(send_audio(message, info_dict,
+                                                  audio_file))
+            while not task.done():
+                await asyncio.sleep(3)
+                await message.reply_chat_action("upload_document")
+            await message.reply_chat_action("cancel")
+            await message.delete()
+    except Exception as e:
+        await message.reply_text(e)
+    await callback_query.message.reply_to_message.delete()
+    await callback_query.message.delete()
+
+
+async def send_audio(message: Message, info_dict, audio_file):
+    basename = audio_file.rsplit(".", 1)[-2]
+    # .webm -> .weba
+    if info_dict['ext'] == 'webm':
+        audio_file_weba = basename + ".weba"
+        os.rename(audio_file, audio_file_weba)
+        audio_file = audio_file_weba
+    # thumbnail
+    thumbnail_url = info_dict['thumbnail']
+    thumbnail_file = basename + "." + \
+        get_file_extension_from_url(thumbnail_url)
+    # info (s2tw)
+    webpage_url = info_dict['webpage_url']
+    title = '@Music24x7SL '+s2tw(info_dict['title'])
+    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+    duration = int(float(info_dict['duration']))
+    performer = s2tw(info_dict['uploader'])
+    await message.reply_audio(audio_file, caption=caption, duration=duration,
+                              performer=performer, title=title,
+                              parse_mode='HTML', thumb=thumbnail_file)
+    os.remove(audio_file)
+    os.remove(thumbnail_file)
+
+
+@Jebot.on_callback_query(filters.regex("^ytdl_video$"))
+async def callback_query_ytdl_video(_, callback_query):
+    try:
+        # url = callback_query.message.text
+        url = callback_query.message.reply_to_message.text
+        ydl_opts = {
+            'format': 'best[ext=mp4]',
+            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
+            'writethumbnail': True
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            message = callback_query.message
+            await message.reply_chat_action("typing")
+            info_dict = ydl.extract_info(url, download=False)
+            # download
+            await callback_query.edit_message_text("**Downloading video...**")
+            ydl.process_info(info_dict)
+            # upload
+            video_file = ydl.prepare_filename(info_dict)
+            task = asyncio.create_task(send_video(message, info_dict,
+                                                  video_file))
+            while not task.done():
+                await asyncio.sleep(3)
+                await message.reply_chat_action("upload_document")
+            await message.reply_chat_action("cancel")
+            await message.delete()
+    except Exception as e:
+        await message.reply_text(e)
+    await callback_query.message.reply_to_message.delete()
+    await callback_query.message.delete()
+
+
+async def send_video(message: Message, info_dict, video_file):
+    basename = video_file.rsplit(".", 1)[-2]
+    # thumbnail
+    thumbnail_url = info_dict['thumbnail']
+    thumbnail_file = basename + "." + \
+        get_file_extension_from_url(thumbnail_url)
+    # info (s2tw)
+    webpage_url = info_dict['webpage_url']
+    title = '@neelathamara_official '+s2tw(info_dict['title'])
+    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+    duration = int(float(info_dict['duration']))
+    width, height = get_resolution(info_dict)
+    await message.reply_video(
+        video_file, caption=caption, duration=duration,
+        width=width, height=height, parse_mode='HTML',
+        thumb=thumbnail_file,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Save ✅",
+                        callback_data="forward_video"
+                    ),
+                   
+                ]
+            ]
+        ))
+    os.remove(video_file)
+    os.remove(thumbnail_file)
+
+
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+def get_resolution(info_dict):
+    if {"width", "height"} <= info_dict.keys():
+        width = int(info_dict['width'])
+        height = int(info_dict['height'])
+    # https://support.google.com/youtube/answer/6375112
+    elif info_dict['height'] == 1080:
+        width = 1920
+        height = 1080
+    elif info_dict['height'] == 720:
+        width = 1280
+        height = 720
+    elif info_dict['height'] == 480:
+        width = 854
+        height = 480
+    elif info_dict['height'] == 360:
+        width = 640
+        height = 360
+    elif info_dict['height'] == 240:
+        width = 426
+        height = 240
+    return (width, height)
+
+
+@Jebot.on_callback_query(filters.regex("^forward_video$"))
+async def callback_query_forward_video(_, callback_query):
+    m_edited = await callback_query.message.edit_reply_markup(None)
+    m_cp = await m_edited.copy(CHANNEL_FORWARD_TO,
+                               disable_notification=True)
+    await callback_query.answer("Saved!")
+    await m_edited.reply(m_cp.link, quote=True)
+
+@Jebot.on_callback_query()
+async def button(bot, update):
+      cb_data = update.data
+      if "help" in cb_data:
+        await update.message.delete()
+        await help(bot, update.message)
+      elif "about" in cb_data:
+        await update.message.delete()
+        await about(bot, update.message)
+      elif "start" in cb_data:
+        await update.message.delete()
+        await start(bot, update.message)
+
+print(
+    """
+Bot Started!
+
+"""
+)
+
+Jebot.run()
